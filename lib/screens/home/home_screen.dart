85fe4bb1fabd66395a24fecb457f8d87c162aade
Paulinhonyx <paulinhonyx@hotmail.com>
2020-06-01 19:35:00 -0300

    Grafico

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -28,7 +28,6 @@ class _HomeState extends State<Home> {
     setState(() {
       _emocao0++;
     });
-    // return makeGroupData(0, _emocao0);
   }
 
   void barGrupo2() {
@@ -41,63 +40,21 @@ class _HomeState extends State<Home> {
     setState(() {
       _emocao2++;
     });
-    // return makeGroupData(0, _emocao0);
   }
 
   void barGrupo4() {
     setState(() {
       _emocao3++;
     });
-    // return makeGroupData(0, _emocao0);
   }
 
   void barGrupo5() {
     setState(() {
       _emocao4++;
     });
-    // return makeGroupData(0, _emocao0);
   }
-  // BarChartGroupData makeGroupData(int x, double y1) {
-  //   return BarChartGroupData(barsSpace: 4, x: x, barRods: [
-  //     BarChartRodData(
-  //       y: y1,
-  //       color: Colors.indigoAccent,
-  //       width: 7,
-  //     ),
-  //     // BarChartRodData(
-  //     //   y: y2,
-  //     //   color: Colors.lightBlueAccent,
-  //     //   width: 7,
-  //     // ),
-  //   ]);
-  // }
-
-  // List<BarChartGroupData> rawBarGroups;
-  // List<BarChartGroupData> showingBarGroups;
 
   @override
-  // void initState() {
-  //   super.initState();
-
-  //   final barGroup1 = makeGroupData(0, 0);
-  //   final barGroup2 = makeGroupData(1, 0);
-  //   final barGroup3 = makeGroupData(2, 0);
-  //   final barGroup4 = makeGroupData(3, 0);
-  //   final barGroup5 = makeGroupData(4, 0);
-
-  //   final items = [
-  //     barGroup1,
-  //     barGroup2,
-  //     barGroup3,
-  //     barGroup4,
-  //     barGroup5,
-  //   ];
-
-  //   rawBarGroups = items.cast<BarChartGroupData>();
-
-  //   showingBarGroups = rawBarGroups;
-  // }
-
   Widget build(BuildContext context) {
     var data = [
       Emotion('Mazelado', _emocao0, Colors.blue),
@@ -280,124 +237,11 @@ class _HomeState extends State<Home> {
                     height: 10,
                   ),
                   Center(
-                    // child: Card(
-                    //   shape: RoundedRectangleBorder(
-                    //     borderRadius: BorderRadius.circular(20),
-                    //   ),
                     child: Padding(
                       padding: EdgeInsets.symmetric(
                         vertical: 20,
                       ),
                       child: chartWidget,
-                      // child: BarChart(
-                      //   BarChartData(
-                      //     maxY: 20,
-                      //     barTouchData: BarTouchData(
-                      //       touchTooltipData: BarTouchTooltipData(
-                      //         tooltipBgColor: Colors.grey,
-                      //         // getTooltipItem: (_a, _b, _c) => null,
-                      //       ),
-                      //     ),
-                      //     titlesData: FlTitlesData(
-                      //       show: true,
-                      //       bottomTitles: SideTitles(
-                      //         showTitles: true,
-                      //         textStyle: TextStyle(
-                      //             color: const Color(0xff7589a2),
-                      //             fontWeight: FontWeight.bold,
-                      //             fontSize: 14),
-                      //         margin: 20,
-                      //         getTitles: (double value) {
-                      //           switch (value.toInt()) {
-                      //             case 0:
-                      //               return 'Mn';
-                      //             case 1:
-                      //               return 'Te';
-                      //             case 2:
-                      //               return 'Wd';
-                      //             case 3:
-                      //               return 'Tu';
-                      //             case 4:
-                      //               return 'Fr';
-                      //             case 5:
-                      //               return 'St';
-                      //             case 6:
-                      //               return 'Sn';
-                      //             default:
-                      //               return '';
-                      //           }
-                      //         },
-                      //       ),
-                      //       leftTitles: SideTitles(
-                      //         showTitles: true,
-                      //         textStyle: TextStyle(
-                      //             color: const Color(0xff7589a2),
-                      //             fontWeight: FontWeight.bold,
-                      //             fontSize: 14),
-                      //         margin: 32,
-                      //         reservedSize: 14,
-                      //         getTitles: (value) {
-                      //           if (value == 0) {
-                      //             return '1K';
-                      //           } else if (value == 10) {
-                      //             return '5K';
-                      //           } else if (value == 19) {
-                      //             return '10K';
-                      //           } else {
-                      //             return '';
-                      //           }
-                      //         },
-                      //       ),
-                      //     ),
-                      //     borderData: FlBorderData(
-                      //       show: false,
-                      //     ),
-                      //     barGroups: showingBarGroups,
-                      //   ),
-                      // ),
-
-                      //LineChart(
-                      //   LineChartData(
-                      //     lineBarsData: <LineChartBarData>[
-                      //       LineChartBarData(
-                      //         spots: [
-                      //           FlSpot(0, 1),
-                      //           FlSpot(1, -1.5),
-                      //           FlSpot(2, -1.4),
-                      //           FlSpot(3, 0.4),
-                      //           FlSpot(4, 2),
-                      //           FlSpot(5, 2),
-                      //           FlSpot(6, 1.8),
-                      //         ],
-                      //         isCurved: true,
-                      //         colors: [
-                      //           Colors.redAccent[400],
-                      //         ],
-                      //         barWidth: 4,
-                      //         isStrokeCapRound: true,
-                      //         dotData: FlDotData(
-                      //           show: false,
-                      //         ),
-                      //         belowBarData: BarAreaData(
-                      //           show: false,
-                      //         ),
-                      //       ),
-                      //     ],
-                      //     minX: 0,
-                      //     minY: -2,
-                      //     maxY: 2,
-                      //     axisTitleData: FlAxisTitleData(
-                      //       leftTitle: AxisTitle(
-                      //         showTitle: true,
-                      //         titleText: 'Coisa',
-                      //       ),
-                      //       bottomTitle: AxisTitle(
-                      //         showTitle: true,
-                      //         titleText: 'Bala',
-                      //       ),
-                      //     ),
-                      //   ),
-                      // ),
                     ),
                     // ),
                   ),
